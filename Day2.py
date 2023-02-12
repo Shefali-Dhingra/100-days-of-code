11365e9f0d3994c8388338fcb56faa2b8af888d8
shefaliDhingra <shefalidhingra060600@gmail.com>
2023-02-12 22:05:22 +0530

    initial commit

---
+++ Day2.py
@@ -0,0 +1,9 @@
+print("Tip Calculator")
+
+bill = float(input("Enter Total bill amount: "))
+
+tip = float(input("Enter Tip Percentage: "))
+
+p = int(input("Enter no. of people to split the bill: "))
+
+print(f'Each Person should pay: {round((bill + (tip*bill) / 100 ),2) / p }')
